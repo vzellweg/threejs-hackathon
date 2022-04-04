8816a3dd55082c31a32558dde9dd8d8cd96cfad9
Victor Zellweger <victor.zellweger@oregonstate.edu>
2022-04-04 15:19:42 -0700

    Complete lesson 5 #3

---
+++ lessons/5/src/script.js
@@ -0,0 +1,58 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "red" }));
+group.add(cube1);
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "cyan" }));
+group.add(cube2);
+const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: "purple" }));
+group.add(cube3);
+
+cube2.position.x = -2;
+cube3.position.x = 2;
+
+// Axes
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+// camera.position.y = 1;
+scene.add(camera);
+
+// camera.lookAt(cube1.position);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
